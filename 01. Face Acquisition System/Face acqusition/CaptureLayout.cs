7b7801067403f7e54bda17e8740ceb7ff1c823cb
devLupin <luckyquit49@gmail.com>
2021-06-29 19:18:45 +0900

    TIL Add OpenCV capture code

---
+++ 01. Face Acquisition System/Face acqusition/CaptureLayout.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Threading;
+using System.Drawing;
+
+namespace Face_acqusition
+{
+    public partial class CaptureLayout : Form
+    {
+        VideoCapture vc;
+        VideoWriter vw = new VideoWriter();
+        
+        private string name;
+        const string path = @"C:\sclab\";
+
+        public CaptureLayout(string name)
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+
+            this.name = name;
+            DirectoryInfo di = new DirectoryInfo(path + name);
+
+            if (di.Exists == false)
+            {
+                di.Create();
+            }
+        }
+
+        private void CaptureLayout_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+
+            try
+            {
+                vc.Dispose();
+            }
+            catch { }
+
+            vc = new VideoCapture(0);
+            timer.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Mat frame = new Mat();
+            vc.Read(frame);
+
+            if(frame.Empty()) { return; }
+            else
+            {
+                try
+                {
+                    this.Invoke((Action)(() =>
+                    {
+                        vw.Write(frame);
+                        pictureBox1.Image = BitmapConverter.ToBitmap(frame);
+                    }), null);
+                }
+                catch { }
+            }
+        }
+
+        private void capture_btn_Click(object sender, EventArgs e)
+        {
+            string cur_filename = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            try
+            {
+                pictureBox1.Image.Save(path + name + @"\" + cur_filename + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                MessageBox.Show("save !");
+            }
+            catch { }
+        }
+
+        private void CaptureLayout_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 01. Face Acquisition System/Face acqusition/StartLayout.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Face_acqusition
+{
+    public partial class StartLayout : Form
+    {
+        public StartLayout()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void Sign_in_btn_Click(object sender, EventArgs e)
+        {
+            Sign_in sign_in = new Sign_in();
+            sign_in.Show();
+        }
+
+        private void sign_up_btn_Click(object sender, EventArgs e)
+        {
+            Sign_up sign_up = new Sign_up();
+            sign_up.Show();
+        }
+    }
+}
