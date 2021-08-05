7ec113e16d950b37cb8068597dcbbd1e4df9b299
devLupin <luckyquit49@gmail.com>
2021-08-05 18:12:31 +0900

    Create Face Detector(ing)

--- # previous(Android)/app/src/main/java/com/example/aquisition/MainActivity.java
+++ # previous(Android)/app/src/main/java/com/example/aquisition/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(!idCheck(userID)) {
                     Toast.makeText(getApplicationContext(), "ID does not exist.", Toast.LENGTH_SHORT).show();
+                    Log.d("", "ABS Path: " + getCacheDir().getPath());
                     return;
 
                 }
