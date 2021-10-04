6a8650a16c91480b31da9744cdcfbdb4a5393f0f
devLupin <luckyquit49@gmail.com>
2021-10-05 06:47:32 +0900

    Error fix

--- Based web app/Face-Detection/script.js
+++ Based web app/Face-Detection/script.js
@@ -1,112 +1,42 @@
-import "./face-api.min.js"
+const video = document.getElementById('video')
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
 
-(function () {
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
-  var width = 320; // We will scale the photo width to this
-  var height = 0; // This will be computed based on the input stream
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
 
-  var streaming = false;
-
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startbutton = null;
-
-  function init() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
-
-    Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models')
-    ]).then(startup)
-  }
-
-  function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
-
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: false
-    })
-      .then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function (err) {
-        console.log("An error occurred: " + err);
-      });
-
-    video.addEventListener('canplay', function (ev) {
-      if (!streaming) {
-        height = video.videoHeight / (video.videoWidth / width);
-
-        if (isNaN(height)) {
-          height = width / (4 / 3);
-        }
-
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
-      }
-    }, false);
-
-    video.addEventListener('play', () => {
-      const canvas = faceapi.createCanvasFromMedia(video)
-      document.body.append(canvas)
-      const displaySize = { width: video.width, height: video.height }
-      faceapi.matchDimensions(canvas, displaySize)
-      setInterval(async () => {
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedDetections)
-        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-      }, 100)
-    })
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    }, 100)
+})
 
-    startbutton.addEventListener('click', function (ev) {
-      takepicture();
-      ev.preventDefault();
-    }, false);
-
-    clearphoto();
-  }
-
-
-  function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
-
-  function takepicture() {
-    var context = canvas.getContext('2d');
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
-      var data = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  init();
-  window.addEventListener('load', startup, false);
-})();
+cameraTrigger.addEventListener("click", function () {
+    cameraSensor.width = video.videoWidth; //640으로 정해져서 나오네?
+    cameraSensor.height = video.videoHeight;
+    cameraSensor.getContext("2d").drawImage(video, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    console.log(cameraSensor.height);
+});
