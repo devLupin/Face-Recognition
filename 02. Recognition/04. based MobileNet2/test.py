f6cc57ffe80b9442e5c8d3d0f5d4814927f6e922
devLupin <luckyquit49@gmail.com>
2022-01-10 21:34:58 +0900

    error fix

--- 02. Recognition/04. based MobileNet2/test.py
+++ 02. Recognition/04. based MobileNet2/test.py
@@ -1,50 +1,60 @@
 #%%
+from sklearn.metrics import accuracy_score
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import os
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import models
 from config import configurations
 
 cfg = configurations[1]
-TRAIN = cfg['TRAIN_PATH']
 TEST = cfg['TEST_PATH']
+
 TARGET_SIZE = cfg['TARGET_SIZE']
 BATCH_SIZE = cfg['BATCH_SIZE']
+EPOCHS = cfg['EPOCHS']
+INPUT_SHAPE = cfg['INPUT_SHAPE']
+
+MODEL_SAVE_PATH = cfg['MODEL_SAVE_PATH']
 
 print("=" * 60)
 print("Overall Configurations:")
 print(cfg)
 print("=" * 60)
 #%%
-from tensorflow.keras import models
-model = models.load_model('Model')
+model = models.load_model(MODEL_SAVE_PATH)
 model.summary()
 
 #%%
-from  tensorflow.keras.preprocessing import image
-import numpy as np
+
 
 def pred(fname, target_size):
     img = image.load_img(fname, target_size=target_size)
     img_tensor = image.img_to_array(img)
-    img_tensor = np.array(img_tensor,dtype="float32")
+    img_tensor = np.array(img_tensor, dtype="float32")
 
     img_tensor /= 255
 
     img_tensor = np.expand_dims(img_tensor, axis=0)
     return img_tensor
 
-#%%
-import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-train_image_generator = ImageDataGenerator(rescale=1./255)
-train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
-                                                           directory=TRAIN,
-                                                           shuffle=True,
-                                                           target_size=TARGET_SIZE,
-                                                           class_mode='categorical')
 
 #%%
-from tqdm import tqdm
 
-train_class = train_data_gen.class_indices
+test_image_generator = ImageDataGenerator(rescale=1./255)
+test_data_gen = test_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                         directory=TEST,
+                                                         shuffle=True,
+                                                         target_size=TARGET_SIZE,
+                                                         class_mode='categorical')
+
+#%%
+
+test_class = test_data_gen.class_indices
 labels = os.listdir(TEST)
 y_pred = []
 y_test = []
@@ -54,19 +64,17 @@ for label in tqdm(labels, desc='predict'):
         x = os.path.join(cur_path, img)
         result = model.predict(pred(x, TARGET_SIZE))
         y_pred.append(np.argmax(result))
-        y_test.append(train_class[label])
+        y_test.append(test_class[label])
 
 #%%
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 cm = confusion_matrix(y_test, y_pred)
-norcm=cm/cm.astype(np.float).sum(axis=0)
+norcm = cm/cm.astype(np.float).sum(axis=0)
 
 #%%
-plt.figure(figsize=(30,30))
-sns.heatmap(norcm,  xticklabels=train_class, yticklabels=train_class, annot=True, fmt='.2%', linewidths=.5)
+plt.figure(figsize=(30, 30))
+sns.heatmap(norcm,  xticklabels=test_class, yticklabels=test_class,
+            annot=True, fmt='.2%', linewidths=.5)
 plt.title('confusion matrix', fontsize=20)
 plt.xlabel('Predicted Label')
 plt.ylabel('True Label')
@@ -75,6 +83,16 @@ plt.show()
 # %%
 print(norcm.shape)
 # %%
-from sklearn.metrics import accuracy_score
-print(accuracy_score(y_true = y_test, y_pred=y_pred))
+print(accuracy_score(y_true=y_test, y_pred=y_pred))
+
+
+# %%
+result = model.predict(pred('19062621.jpg', TARGET_SIZE))
+labels = os.listdir(TEST)
+y_pred = np.argmax(result)
+print(result[0][y_pred])
+print()
+print(labels[np.argmax(result)])
+# y_pred.append(np.argmax(result))
+# y_test.append(train_class[label])
 # %%
--- 02. Recognition/04. based MobileNet2/train.py
+++ 02. Recognition/04. based MobileNet2/train.py
@@ -1,4 +1,14 @@
 #%%
+import tensorflow_addons as tfa
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
 from config import configurations
 
 cfg = configurations[1]
@@ -9,6 +19,9 @@ BATCH_SIZE = cfg['BATCH_SIZE']
 EPOCHS = cfg['EPOCHS']
 INPUT_SHAPE = cfg['INPUT_SHAPE']
 
+CHECK_POINT = cfg['CHECK_POINT']
+MODEL_SAVE_PATH = cfg['MODEL_SAVE_PATH']
+
 print("=" * 60)
 print("Overall Configurations:")
 print(cfg)
@@ -17,12 +30,11 @@ print("=" * 60)
 #%%
 # Data preparing
 
-import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 classes = os.listdir(TRAIN)
 
-train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
+train_image_generator = ImageDataGenerator(
+    rescale=1./255)  # Generator for our training data
 val_image_generator = ImageDataGenerator(rescale=1./255)
 
 train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
@@ -36,41 +48,38 @@ val_data_gen = val_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
                                                        target_size=TARGET_SIZE,
                                                        class_mode='categorical')
 
-print(len(train_data_gen.class_indices))
+num_of_samples = train_data_gen.samples
+print(num_of_samples)
 # %%
 # Image displaying
 
 sample_training_images, _ = next(train_data_gen)
 # %%
-import matplotlib.pyplot as plt
+
 
 def plot_images(img_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
     axes = axes.flatten()
-    
+
     for img, ax in zip(img_arr, axes):
         ax.imshow(img)
         ax.axis('off')
-    
+
     plt.tight_layout()
     plt.show()
 
+
 plot_images(sample_training_images[:5])
 # %%
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.optimizers import Adam, SGD
 
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 # %%
 model = Sequential()
 model.add(MobileNetV2(input_shape=INPUT_SHAPE,
-                    include_top=False,
-                    weights='imagenet'))
+                      include_top=False,
+                      weights='imagenet'))
 model.add(GlobalAveragePooling2D())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(400, activation='softmax'))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(800, activation='softmax'))
 #%%
 model.compile(optimizer=SGD(learning_rate=0.001, momentum=0.85),
               loss='categorical_crossentropy',
@@ -78,22 +87,20 @@ model.compile(optimizer=SGD(learning_rate=0.001, momentum=0.85),
 # %%
 model.summary()
 # %%
-from tensorflow.keras.utils import plot_model
 plot_model(model, show_shapes=True)
 
 # %%
-import tensorflow_addons as tfa
 
 stop = EarlyStopping(patience=5)
-checkpoint = ModelCheckpoint("checkpoint/cp.ckpt",monitor='val_accuracy',
-                            save_weights_only=True, mode='max',verbose=1,save_best_only=True)
+checkpoint = ModelCheckpoint(CHECK_POINT, monitor='val_accuracy',
+                             save_weights_only=True, mode='max', verbose=1, save_best_only=True)
 tqdm_callback = tfa.callbacks.TQDMProgressBar()
 # callbacks = [stop, checkpoint, tqdm_callback]
 callbacks = [checkpoint]
 # %%
 history = model.fit(train_data_gen,
-                    validation_data = val_data_gen,
-                    steps_per_epoch=int(3600/BATCH_SIZE),
+                    validation_data=val_data_gen,
+                    steps_per_epoch=int(num_of_samples/BATCH_SIZE),
                     callbacks=callbacks,
                     epochs=EPOCHS)
 # %%
@@ -120,5 +127,5 @@ plt.legend()
 
 plt.show()
 # %%
-model.save('Model')
+model.save(MODEL_SAVE_PATH)
 # %%
