e28ce09d632d87993acb6dd583e23db3afa0ae26
devLupin <luckyquit49@gmail.com>
2022-02-08 05:22:17 +0900

    Fix error that ckpt save code

--- 02. Recognition/05. Real-time Face recognition/train.py
+++ 02. Recognition/05. Real-time Face recognition/train.py
@@ -1,17 +1,16 @@
-from silence_tensorflow import silence_tensorflow
-silence_tensorflow()
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-from absl import app, logging
-import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
-
-from modules.models import ArcFaceModel
-from modules.losses import SoftmaxLoss
-from modules.utils import set_memory_growth, load_yaml, get_ckpt_inf
 import modules.dataset as dataset
+from modules.utils import set_memory_growth, load_yaml, get_ckpt_inf
+from modules.losses import SoftmaxLoss
+from modules.models import ArcFaceModel
+import tensorflow as tf
+from absl import app, logging
+
+from silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+
 
 def main(_):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -28,16 +27,15 @@ def main(_):
                          embd_shape=cfg['embd_shape'],
                          w_decay=cfg['w_decay'],
                          training=True)
-    model.summary()
+    model.summary(line_length=80)
 
-
+    # train dataset
     logging.info("load K-Face dataset.")
     dataset_len = cfg['num_samples']
     steps_per_epoch = dataset_len // cfg['batch_size']
     train_dataset = dataset.load_tfrecord_dataset(
-            cfg['train_dataset'], cfg['batch_size'], cfg['binary_img'],
-            is_ccrop=cfg['is_ccrop'])
-
+        cfg['train_dataset'], cfg['batch_size'], cfg['binary_img'],
+        is_ccrop=cfg['is_ccrop'])
 
     learning_rate = tf.constant(cfg['base_lr'])
     optimizer = tf.keras.optimizers.SGD(
@@ -53,31 +51,50 @@ def main(_):
         print("[*] training from scratch.")
         epochs, steps = 1, 1
 
+    train_dataset = iter(train_dataset)
+    summary_writer = tf.summary.create_file_writer("logs/" + cfg['sub_name'])
 
-    model.compile(optimizer=optimizer, loss=loss_fn)
-    
-    mc_callback = ModelCheckpoint(
-        'checkpoints/' + cfg['sub_name'] + '/e_{epoch}_b_{batch}.ckpt',
-        save_freq=cfg['save_steps'] * cfg['batch_size'], verbose=1,
-        save_weights_only=True)
-    
-    tb_callback = TensorBoard(log_dir='logs/',
-                              update_freq=cfg['batch_size'] * 5,
-                              profile_batch=0)
-    
-    tb_callback._total_batches_seen = steps
-    tb_callback._samples_seen = steps * cfg['batch_size']
-    callbacks = [mc_callback, tb_callback]
-    
-    model.fit(train_dataset,
-              epochs=cfg['epochs'],
-              steps_per_epoch=steps_per_epoch,
-              callbacks=callbacks,
-              initial_epoch=epochs - 1)
+    # training loop
+    while epochs <= cfg['epochs']:
+        inputs, labels = next(train_dataset)
+
+        with tf.GradientTape() as tape:
+            logits = model(inputs, training=True)
+            reg_loss = tf.reduce_sum(model.losses)
+            pred_loss = loss_fn(labels, logits)
+            total_loss = pred_loss + reg_loss
+
+        grads = tape.gradient(total_loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        if steps % (cfg['save_steps'] // 10) == 0:
+            verb_str = "Epoch {}/{}: {}/{}, loss={:.2f}, lr={:.4f}"
+            print(verb_str.format(epochs, cfg['epochs'],
+                                  steps % steps_per_epoch,
+                                  steps_per_epoch,
+                                  total_loss.numpy(),
+                                  learning_rate.numpy()))
+
+            with summary_writer.as_default():
+                tf.summary.scalar(
+                    'loss/total loss', total_loss, step=steps)
+                tf.summary.scalar(
+                    'loss/pred loss', pred_loss, step=steps)
+                tf.summary.scalar(
+                    'loss/reg loss', reg_loss, step=steps)
+                tf.summary.scalar(
+                    'learning rate', optimizer.lr, step=steps)
+
+        if steps % cfg['save_steps'] == 0:
+            print('[*] save ckpt file!')
+            model.save_weights('checkpoints/{}/e_{}_b_{}.ckpt'.format(
+                cfg['sub_name'], epochs, steps % steps_per_epoch))
+
+        steps += 1
+        epochs = steps // steps_per_epoch + 1
 
     print("[*] training done!")
 
-    model.save('Model')
 
 if __name__ == '__main__':
     app.run(main)
